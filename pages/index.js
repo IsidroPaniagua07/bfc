3ec5f45e71d48efe1db45d1a088b06e8346ea370
Isidro Paniagua <107605919+IsidroPaniagua07@users.noreply.github.com>
2022-11-10 23:39:23 -0800

    initial homepage layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div className="home-wrapper">
-      <div className="home-container">asd</div>
-    </div>
+    <>
+
+      <div className="home-wrapper">
+        <div className="home-container">
+          <div className="information-container">
+
+          <h3 className="home-header">Boni Fiscal</h3>
+          <p className="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lect
+          us magna fringilla urna porttitor rhoncus dolor purus non enim. Dictum non consectetur a erat nam at lectus. Morbi tincidunt augue interdum velit euismod in.</p>
+          </div>
+          <div className="footer-container">
+
+          <p>Se habla Español!</p>
+          <p>619-123-4567</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
