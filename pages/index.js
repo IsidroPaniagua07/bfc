2870a2eabcca328dff5324141e74d7fa5a264a04
Isidro <IsidroPaniagua07@gmail.com>
2022-11-23 01:44:15 -0800

    added image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { FaEnvelope } from "react-icons/fa";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center h-[100vh] w-full">
-        <div className="flex flex-col justify-center items-center my-10 text-2xl font-lora text-gray-500">
+      <div className="flex flex-col items-center h-full w-full p-4">
+        <div className="flex flex-col h-[10%] justify-center items-center text-2xl font-lora text-gray-500">
           <p className="flex gap">
             <BsFillTelephoneFill size={30} />
             (619)381-8593
@@ -15,22 +16,40 @@ export default function Home() {
             BoniFacioF12@gmail.com
           </p>
         </div>
-        <div className="flex flex-col w-4/5 h-4/5 ">
+        <div className="flex flex-col w-4/5 h-[90%] ">
           <div className="flex h-full flex-col">
-            <h3 className="home-header header-fade underline decoration-[#960018] decoration-[6px] gap-0 leading-none">Fiscal & Co. <br/>  </h3>
-            <div className="font-julius text-6xl header-fade leading-none">
-            Construction
+            <div>
+              <h3 className="home-header header-fade underline decoration-[#960018] decoration-[6px] gap-0 leading-none">
+                Fiscal & Co. <br />{" "}
+              </h3>
+              <div className="font-julius text-6xl header-fade leading-none">
+                Construction
+              </div>
+              <div className="flex justify-end">
+                <p className="home-description h-full w-1/2 description-fade text-2xl ">
+                  Honesty, reliability, and superior craftsmanship.
+                </p>
+              </div>
             </div>
-            <div className="flex justify-end">
-              <p className="home-description h-full w-1/2 description-fade text-2xl ">
-                Honesty, reliability, and superior craftsmanship.
-              </p>
+            <div className="flex h-full w-full justify-center items-center">
+              <div className="flex h-4/5 w-[50%] border-black border relative">
+                <Image
+                  src="/1.jpg"
+                  alt="landscaping"
+                  layout="responsive"
+                  height={1246}
+                  width={1242}
+                />
+              </div>
+            </div>
+            <div className="text-3xl">
+              Fiscal & Co. Construction is a premiere general company located in San Diego, CA. We specialize in new construction 
             </div>
           </div>
-          <div className="absolute bottom-[10%] left-[44%] flex flex-col justify-center items-center h-10 text-2xl footer-fade">
-            {/* <p>Se habla Español!</p> */}
-            {/* <p>619-123-4567</p> */}
-          </div>
+          {/* <div className="absolute bottom-[10%] left-[44%] flex flex-col justify-center items-center h-10 text-2xl footer-fade">
+            <p>Se habla Español!</p>
+            <p>619-123-4567</p>
+          </div> */}
         </div>
       </div>
     </>
