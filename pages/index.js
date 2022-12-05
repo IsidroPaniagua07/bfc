08b8ce0110c67b175820d5b81bbac72b9ccf59bd
Isidro Paniagua <107605919+IsidroPaniagua07@users.noreply.github.com>
2022-12-04 23:54:30 -0800

    implemented carousel

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,47 @@
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { FaEnvelope } from "react-icons/fa";
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  let slider;
+  let current;
+  let prev;
+  let next;
+
+  const move = (dir) => {
+    const classesToRemove = ["prev", "current", "next"];
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+    if (dir === "left")
+      [prev, current, next] = [
+        prev.previousElementSibling || slider.lastElementChild,
+        prev,
+        current,
+      ];
+    if (dir === "right")
+      [prev, current, next] = [
+        current,
+        next,
+        next.nextElementSibling || slider.firstElementChild,
+      ];
+    applyClasses();
+  };
+
+  const applyClasses = () => {
+    current.classList.add("current");
+    prev.classList.add("prev");
+    next.classList.add("next");
+  };
+
+  if (typeof window !== "undefined") {
+    slider = document.querySelector(".slider");
+    current = slider.querySelector(".current") || slider.firstElementChild;
+    prev = current.previousElementSibling || slider.lastElementChild;
+    next = current.nextElementSibling || slider.firstElementChild;
+  }
+
   return (
     <>
       <div className="flex flex-col items-center h-full w-full p-4">
@@ -16,54 +55,66 @@ export default function Home() {
             BoniFacioF12@gmail.com
           </p>
         </div>
-        <div className="flex flex-col w-4/5 h-[90%] ">
-          <div className="flex h-full flex-col">
-            <div>
-              <h3 className="home-header header-fade underline decoration-[#960018] decoration-[6px] gap-0 leading-none">
-                Fiscal & Co. <br />{" "}
-              </h3>
-              <div className="font-julius text-6xl header-fade leading-none">
-                Construction
+          <div className="flex h-full w-[80%] flex-col justify-center">
+            <div className="flex flex-row">
+              <div className="flex flex-col w-1/2">
+                <h3 className="home-header header-fade underline decoration-[#960018] decoration-[6px] gap-0 leading-none">
+                  Fiscal & Co. <br />{" "}
+                </h3>
+                <div className="font-julius text-6xl header-fade leading-none">
+                  Construction
+                </div>
               </div>
-              <div className="flex justify-end">
-                <p className="home-description h-full w-1/2 description-fade text-2xl ">
+              <div className="w-1/2 relative overflow-hidden">
+                <h2 className="flex home-description h-full description-fade text-2xl justify-center items-center">
                   Honesty, reliability, and superior craftsmanship.
-                </p>
+                </h2>
               </div>
             </div>
-            <div className="flex h-full w-full justify-center items-center">
-              <div className="flex h-4/5 w-[50%] border-black border relative overflow-hidden">
-                <Image
-                  className="slide prev"
-                  src="/1.jpg"
-                  alt="landscaping"
-                  layout="intrinsic"
-                  height={1246}
-                  width={1242}
+            <div className="flex h-[650px] w-full justify-center items-center">
+              <div className="flex h-[623px] w-[621px] relative justify-center items-center text-center ">
+                <button
+                  className="carousel-button left"
+                  onClick={() => move("left")}
+                >&#8249;</button>
+                <button
+                  className="carousel-button right"
+                  onClick={() => move("right")}
+                >
+                  &#8250;
+                </button>
+                <div className="h-full w-full relative slider rounded-[3rem]">
+                  <Image
+                    className="slide prev"
+                    src="/2.jpg"
+                    alt="construction"
+                    layout="intrinsic"
+                    height={623}
+                    width={621}
                   />
-                <Image
-                  className="slide current"
-                  src="/1.jpg"
-                  alt="landscaping"
-                  layout="intrinsic"
-                  height={1246}
-                  width={1242}
+                  <Image
+                    className="slide current"
+                    src="/1.jpg"
+                    alt="construction"
+                    layout="intrinsic"
+                    height={623}
+                    width={621}
                   />
-                <Image
-                  className="slide next" 
-                  src="/1.jpg"
-                  alt="landscaping"
-                  layout="intrinsic"
-                  height={1246}
-                  width={1242}
-                />
-              </div>
+                  <Image
+                    className="slide next"
+                    src="/2.jpg"
+                    alt="construction"
+                    layout="intrinsic"
+                    height={623}
+                    width={621}
+                  />
+                </div>
             </div>
-            <div className="text-3xl">
+          </div>
+            <div className="text-3xl mt-58">
               Fiscal & Co. Construction is a premiere general company located in
               San Diego, CA. We specialize in new construction
             </div>
-          </div>
           {/* <div className="absolute bottom-[10%] left-[44%] flex flex-col justify-center items-center h-10 text-2xl footer-fade">
             <p>Se habla Español!</p>
             <p>619-123-4567</p>
